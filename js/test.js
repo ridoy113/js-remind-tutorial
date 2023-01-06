5a7d994e26539ea65286dd5c3402a8415e8de810
ridoy113 <almamounhridoy113@gmai.com>
2023-01-06 22:06:58 +0600

    try for of loop

--- js/test.js
+++ js/test.js
@@ -1,4 +1,14 @@
-const product1 = 1;
-const product2 = "0";
+// const product1 = 1;
+// const product2 = "0";
 
-console.log(product1 + product2);
+// console.log(product1 + product2);
+
+
+
+
+let a ="sakib";
+
+for (let x of a){
+    console.log(x);
+}
+
